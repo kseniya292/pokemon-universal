1ce2696aa37188deb6a8c895d198081c308553e5
Kseniya Lifanova <kseniya.hogan@gmail.com>
2018-01-16 12:31:30 -0800

    added meta data

--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { PokemonService } from '../pokemon.service';
 import { Pokemon } from '../pokemon';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pokemon-details',
@@ -15,7 +16,22 @@ export class PokemonDetailsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private pokemonService: PokemonService) { }
+    private pokemonService: PokemonService,
+    public meta: Meta,
+    public title: Title) {
+      title.setTitle('Pokemon Detail');
+      meta.addTags([
+        {
+          name: 'author', content: 'Kseniya'
+        },
+        {
+          name: 'keywords', content: 'Detailed Pokemon'
+        },
+        {
+          name: 'description', content: 'Detailed info about pokemon'
+        }
+      ]);
+     }
 
   ngOnInit() {
     this.route.paramMap.subscribe((params) => {
